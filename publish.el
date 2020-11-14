bada670a3084a14c9f14f1dfbf37378ced066bb2
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2020-11-13 20:59:53 -0500

    changes to the publishing code

--- publish.el
+++ publish.el
@@ -7,52 +7,42 @@
 (require 'ox-publish)
 
 ;;; Code:
-(defconst base-dir (file-name-directory load-file-name))
-(defconst site-publish-dir (concat base-dir "/public"))
-(defconst site-source-dir (concat base-dir "/src"))
+(defconst base-dir (file-name-directory (or load-file-name (buffer-file-name (current-buffer)))))
+(defconst site-publish-dir (concat base-dir "public"))
+(defconst site-source-dir (concat base-dir "src"))
 
 ;;; Extra Packages
-(add-to-list 'load-path (concat  base-dir "/lisp"))
-
-;;; HTML Styles
-;; use styles for HTMLizing
-(setq org-html-htmlize-output-type 'css)
+(add-to-list 'load-path (concat  base-dir "lisp"))
 
 ;;; Fonts
-
 ;; <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald&display=swap" rel="stylesheet">
 
-;;; Customizations
-(defun custom-html-tags (tags info)
-  (when tags
-    (format "<p class=\"tag\">%s</p>"
-	    (mapconcat
-	     (lambda (tag)
-	       (format "<a href=\"%s\"><span class=\"label\">%s</span></a>"
-                       (format  "/projects/tagged/%s/" tag)
-		       ;; (concat (plist-get info :html-tag-class-prefix)
-		       ;;         (org-html-fix-class-name tag))                       
-		       tag))
-	     tags ""))))
+;;; Org HTML Setup
+(setf org-html-doctype "html5"
+      org-html-html5-fancy t
+      ;; use styles for HTMLizing
+      org-html-htmlize-output-type 'css
+      )
 
-(defun custom-org-html-format-headline-function
-    (todo _todo-type priority text tags info)
-  "Custom format function for a headline.
-See `org-html-format-headline-function' for details."
-  (let ((todo (org-html--todo todo info))
-	(priority (org-html--priority priority info))
-	(tags (custom-html-tags tags info)))
-    (concat todo (and todo " ")
-	    priority (and priority " ")
-	    text
-	    (and tags "&#xa0;&#xa0;&#xa0;") tags)))
-
-(setq org-html-format-headline-function #'custom-org-html-format-headline-function)
-
-(setq org-html-doctype "xhtml5")
 
 ;;; Publishing Project 
-(setq org-publish-project-alist
+
+;; #+OPTIONS: auto-id:t toc:t tags:t
+;; #+options: html-link-use-abs-url:nil html-postamble:auto
+;; #+options: html-preamble:t html-scripts:t html-style:t
+;; #+options: html5-fancy:t tex:t
+;; #+html_container: div
+;; #+description:
+;; #+keywords:
+;; #+html_head:
+;; #+html_head_extra:
+;; #+subtitle:
+;; #+infojs_opt:
+;; #+creator: <a href="https://www.gnu.org/software/emacs/">Emacs</a> 26.1 (<a href="https://orgmode.org">Org</a> mode 9.3.6)
+;; #+latex_header:
+
+
+(setf org-publish-project-alist
       `(
         ("site" :components ("site-content" "site-static"))
         ("site-content"
@@ -60,19 +50,24 @@ See `org-html-format-headline-function' for details."
          :html-extension "html"
          :base-extension "org"
          :publishing-directory ,site-publish-dir
-         :publishing-function (org-html-publish-to-html)
+         :publishing-function org-html-publish-to-html
          :htmlized-source t
          :recursive t          ; descend into sub-folders?
          :section-numbers nil  ; don't create numbered sections
          :with-toc nil         ; don't create a table of contents
          :with-latex t         ; do use MathJax for awesome formulas!
-        ; :html-head-extra ""   ; extra <head> entries go here
-        ; :html-preamble ""     ; this stuff is put before your post
-        ; :html-postamble ""    ; this stuff is put after your post         
+         :html-link-home "/"
+
+         ;; sitemap
+         :auto-sitemap t
+         :sitemap-filename "site.org"
+         :sitemap-title "Site"
+         :sitemap-sort-files anti-chronologically
+         :sitemap-date-format "Published %d %b %d %Y"
          )
         ("site-static"
          :base-directory ,(concat  base-dir "/static")
          :base-extension "*"
          :recursive t
-	 :publishing-function (org-publish-attachment)
+	 :publishing-function org-publish-attachment
          )))

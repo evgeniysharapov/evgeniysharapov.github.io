bd688dfadb5667c5afb847783498ef2306481e52
Evgeniy Sharapov <evgeniy.sharapov@gmail.com>
2020-11-17 20:34:28 -0500

    updates

--- publish.el
+++ publish.el
@@ -10,6 +10,7 @@
 (defconst base-dir (file-name-directory (or load-file-name (buffer-file-name (current-buffer)))))
 (defconst site-publish-dir (concat base-dir "public"))
 (defconst site-source-dir (concat base-dir "src"))
+(defconst site-assets-dir (concat base-dir "assets"))
 
 ;;; Extra Packages
 (add-to-list 'load-path (concat  base-dir "lisp"))
@@ -41,11 +42,19 @@
 ;; #+creator: <a href="https://www.gnu.org/software/emacs/">Emacs</a> 26.1 (<a href="https://orgmode.org">Org</a> mode 9.3.6)
 ;; #+latex_header:
 
+;; (defun blog-sitemap-function (project &optional sitemap-filename)
+;;   "Generates sitemap for the blog"
+;;   (let* ((projects-props (cdr project))
+;;          (dir (file-name-as-directory (plist-get project-props :base-directory)))
+;;          )
+;;     (message ))
+;;   )
 
 (setf org-publish-project-alist
       `(
-        ("site" :components ("site-content" "site-static"))
-        ("site-content"
+        ("homepage" :components ("content" "blog" "assets"))
+        ; main content with projects, articles, etc
+        ("content"
          :base-directory ,site-source-dir
          :html-extension "html"
          :base-extension "org"
@@ -57,16 +66,32 @@
          :with-toc nil         ; don't create a table of contents
          :with-latex t         ; do use MathJax for awesome formulas!
          :html-link-home "/"
-
+         :exclude "^blog"
+         )
+        ; blog content has sitemap that is a list of posts
+        ("blog"
+         :base-directory ,(concat site-source-dir "/blog")
+         :html-extension "html"
+         :base-extension "org"
+         :publishing-directory ,(concat site-publish-dir "/blog")
+         :publishing-function org-html-publish-to-html
+         :htmlized-source t
+         :recursive t          ; descend into sub-folders?
+         :section-numbers nil  ; don't create numbered sections
+         :with-toc nil         ; don't create a table of contents
+         :with-latex t         ; do use MathJax for awesome formulas!
+         :html-link-home "/"         
          ;; sitemap
          :auto-sitemap t
-         :sitemap-filename "site.org"
-         :sitemap-title "Site"
+         :sitemap-filename "index.org"
+         :sitemap-title "Blog"
          :sitemap-sort-files anti-chronologically
+         :sitemap-function blog-sitemap-function
          :sitemap-date-format "Published %d %b %d %Y"
          )
-        ("site-static"
-         :base-directory ,(concat  base-dir "/static")
+        ; assets, css/js/images/etc
+        ("assets"
+         :base-directory ,site-assets-dir
          :base-extension "*"
          :recursive t
 	 :publishing-function org-publish-attachment

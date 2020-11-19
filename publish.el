6e741fa86c46ef7c9939eee96c45f3c0119ea5c8
Evgeniy N. Sharapov <evgeniy.sharapov@gmail.com>
2020-11-18 23:29:37 -0500

    somewhat workable simple version

--- publish.el
+++ publish.el
@@ -16,11 +16,41 @@
 (defconst site-publish-dir (concat base-dir "public"))
 (defconst site-source-dir (concat base-dir "src"))
 (defconst site-assets-dir (concat base-dir "assets"))
+
 ;;;; Dates Formatting
 (defvar publish-date-format "%h %d, %Y"
   "Format for displaying publish dates.")
 (defvar sitemap-date-format "Published %d %b %d %Y"
   "Format dates for the list of blog posts (sitemap).")
+
+;;;; HTML includes
+(defvar site-html-head
+  "<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,400italic' rel='stylesheet' type='text/css'>
+<link rel='stylesheet' href='/assets/css/site.css' type='text/css'/>")
+
+(defvar site-html-blog-head
+  "<link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,400italic' rel='stylesheet' type='text/css'>
+<link rel='stylesheet' href='/assets/css/custom.css' type='text/css'/>
+<link rel='stylesheet' href='/assets/css/site.css' type='text/css'/>")
+
+(defvar site-html-preabmble
+  "<div class='nav'>
+<ul>
+<li><a href='/'>Home</a></li>
+<li><a href='/blog/'>Blog</a></li>
+<li><a href='http://github.com/evgeniysharapov'>GitHub</a></li>
+<li><a href='http://twitter.com/evgeniysharapov'>Twitter</a></li>
+<li><a href='/contact.html'>Contact</a></li>
+</ul>
+</div>")
+
+(defvar site-html-postamble
+"<div class='footer'>
+Copyright 2013 %a (%v HTML).<br>
+Last updated %C. <br>
+Built with %c.
+</div>")
+
 ;;;; User
 (setf user-full-name "Evgeniy N. Sharapov"
       user-mail-address "evgeniy.sharapov@gmail.com")
@@ -37,36 +67,36 @@
 
 ;;; Default Export Settings
 ;; They can be overriden on file/project basis
-(setf org-export-with-inlinetasks nil
-      org-export-with-section-numbers nil
-      org-export-with-smart-quotes t
-      org-export-with-statistics-cookies nil
-      org-export-with-toc nil
-      org-export-with-tasks nil)
+;; (setf org-export-with-inlinetasks nil
+;;       org-export-with-section-numbers nil
+;;       org-export-with-smart-quotes t
+;;       org-export-with-statistics-cookies nil
+;;       org-export-with-toc nil
+;;       org-export-with-tasks nil)
 
 ;;; Org HTML Setup
-(setf org-html-doctype "html5"
-      org-html-html5-fancy t
-      ;; use styles for HTMLizing
-      org-html-htmlize-output-type 'css
-      org-html-checkbox-type 'html
-      org-html-divs '((preamble  "header" "top")
-                      (content   "main"   "content")
-                      (postamble "footer" "postamble"))
-      org-html-container-element "section"
-      org-html-metadata-timestamp-format "%Y-%m-%d"
-      org-html-head-include-default-style t
-      org-html-style-default (site-snippet-file-to-string "header.html")
-      org-html-head-include-scripts t
-      org-html-scripts (site-snippet-file-to-string "scripts.html")
-      org-html-home/up-format "%s\n%s\n"
-)
+;; (setf org-html-doctype "html5"
+;;       org-html-html5-fancy t
+;;       ;; use styles for HTMLizing
+;;       org-html-htmlize-output-type 'css
+;;       org-html-checkbox-type 'html
+;;       org-html-divs '((preamble  "header" "top")
+;;                       (content   "main"   "content")
+;;                       (postamble "footer" "postamble"))
+;;       org-html-container-element "section"
+;;       org-html-metadata-timestamp-format "%Y-%m-%d"
+;;       org-html-head-include-default-style t
+;;       org-html-style-default (site-snippet-file-to-string "header.html")
+;;       org-html-head-include-scripts t
+;;       org-html-scripts (site-snippet-file-to-string "scripts.html")
+;;       org-html-home/up-format "%s\n%s\n"
+;; )
 
 
 ;;; Publishing Project 
 (setf org-publish-project-alist
       `(
-        ("homepage" :components ("content" "blog" "assets"))
+        ("evgeniysharapov.com" :components ("content" "blog" "assets"))
         ; main content with projects, articles, etc
         ("content"
          :base-directory ,site-source-dir
@@ -75,18 +105,21 @@
          :publishing-directory ,site-publish-dir
          :publishing-function org-html-publish-to-html
          :recursive t          ; descend into sub-folders?
+
          ;; Content of each file
          :section-numbers nil  ; don't create numbered sections
-         :headline-level 4
-         :with-author t
-         :with-creator nil
-         :with-toc t         
-         :with-latex t         ; do use MathJax for awesome formulas!
-         :htmlized-source t
-         
-         :html-link-home "/"
+         ;:headline-level 4
+         ;:with-author t
+         ;:with-creator nil
+         :with-toc nil         
+         ;:with-latex t         ; do use MathJax for awesome formulas!
+         ;:htmlized-source t
+         ;:html-link-home "/"
          :exclude "^blog"
-         
+
+         :html-head ,site-html-head
+         :html-preamble ,site-html-preabmble
+         :html-postamble ,site-html-postamble
          )
         ; blog content has sitemap that is a list of posts
         ("blog"
@@ -100,10 +133,13 @@
          ;; Content of each file
          :section-numbers nil  ; don't create numbered sections
          :with-toc nil         ; don't create a table of contents
-         :with-latex t         ; do use MathJax for awesome formulas!
-         :htmlized-source t
+         ;:with-latex t         ; do use MathJax for awesome formulas!
+         ;:htmlized-source t
 
-         :html-link-home "/"         
+         ; :html-link-home "/"         
+         :html-head ,site-html-blog-head
+         :html-preamble ,site-html-preabmble
+         :html-postamble ,site-html-postamble
          
          ;; Sitemap
          :auto-sitemap t
@@ -117,7 +153,7 @@
         ("assets"
          :base-directory ,site-assets-dir
          :publishing-directory ,(concat site-publish-dir "/assets")
-         :base-extension "*"
+         :base-extension "css"
          :recursive t
 	 :publishing-function org-publish-attachment
          )))
